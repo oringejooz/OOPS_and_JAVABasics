6f72ca3799ffcf5bdc29d9f0ec0f4d748528ae2e
Swarnima <bishtswarnima@gmail.com>
2023-12-11 21:58:35 +0530

    modified:   Obj_Oriented/final_keyword/Final_methods.java

--- Obj_Oriented/final_keyword/Final_methods.java
+++ Obj_Oriented/final_keyword/Final_methods.java
@@ -1,14 +1,31 @@
 package Obj_Oriented.final_keyword;
 
+// final class Calc{
+//     public void show(){
+//         System.out.println("in Calc show");
+//     }
+//     public void add(int a,int b){
+//         System.out.println(a+b);
+//     }
+// } - final keyword prevents inheritance
+
 class Calc{
-    public void show(){
-        System.out.println("in Calc show");
+    public final void show(){
+        System.out.println("By Swar");
     }
     public void add(int a,int b){
         System.out.println(a+b);
     }
 }
 
-public class Final_methods {
+class AdvCalc extends Calc{
     
+}
+//Method Overriding - Child class overrides the method of the Parent class. To prevent this - final keyword is used
+public class Final_methods {
+    public static void main(String[] args) {
+        AdvCalc c = new AdvCalc();
+        c.show();
+        c.add(3, 5);
+    }
 }
