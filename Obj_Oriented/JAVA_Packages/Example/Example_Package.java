7e3b9e3a0b4c7ebdb19fb3ff58fa7fb3e644cc56
Swarnima <bishtswarnima@gmail.com>
2023-12-11 19:58:17 +0530

    Packages

---
+++ Obj_Oriented/JAVA_Packages/Example/Example_Package.java
@@ -0,0 +1,9 @@
+package Obj_Oriented.JAVA_Packages.Example;
+
+import Obj_Oriented.JAVA_Packages.bank.Bank;
+import bank.*;
+
+public class Example_Package {
+    Bank ac1 = new Bank();
+    bank.Account = new 
+}
---
+++ Obj_Oriented/JAVA_Packages/bank/Bank.java
@@ -0,0 +1,9 @@
+package Obj_Oriented.JAVA_Packages.bank;
+
+class Account{
+    public String name;
+}
+
+public class Bank {
+    
+}
